41f07a39467bb4ee009f8926099bba5deb675cad
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2021-12-08 21:21:46 +0330

    click event of vertical menu edited

--- source/script/main.js
+++ source/script/main.js
@@ -1,13 +1,14 @@
 // for add scroll to menu
-let menuLink = document.querySelector(".menu");
 
+let menuLink = document.querySelector(".menu");
 for (let i = 0; i < menuLink.children.length; i++) {
     /*
      if user clicks on every item of menu
      page scrolls to that element with this event listener
     */
-    menuLink.children[i].addEventListener("click", function () {
-        document.getElementById("c" + (i + 1)).scrollIntoView({
+    menuLink.children[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelectorAll(".card-wrapper")[i].scrollIntoView({
             behavior: 'smooth'
         });
     });
